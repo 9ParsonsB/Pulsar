c0c69dcdf79fff54bbf0ddad37cc3ef684d46312
Ben Parsons <9parsonsb@gmail.com>
2024-04-14 21:48:31 +1000

    Add Initial WebApp

---
+++ Pulsar/WebApp/src/routes/+layout.svelte
@@ -0,0 +1,32 @@
+<nav>
+    <ul>
+        <li><a href="/">Dashboard</a></li>
+        <li><a href="/ship">Ship</a></li>
+        <li><a href="/botany">Botanist</a></li>
+        <li><a href="/explorer">Explorer</a></li>
+        <li><a href="/options">Options</a></li>
+    </ul>
+</nav>
+
+<slot />
+
+<style>
+    ul {
+        display: flex;
+        list-style-type: none;
+    }
+    li {
+        padding: 1%;
+    }
+
+    :global(body) {
+        background-color: black;
+        color: white;
+        [href] {
+            color: red;
+            &:visited {
+                color: aqua;
+            }
+        }
+    }
+</style>
---
+++ Pulsar/WebApp/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let x = $state(1)
+
+    const increment = () => {
+        x = x + 1;
+    }
+</script>
+
+<h1>Welcome to Pulsar</h1>
+
+<button on:click={increment} > Increment </button>
+
+<span> {x} </span>
